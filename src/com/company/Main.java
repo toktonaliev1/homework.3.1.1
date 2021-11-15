14db04532f7ef155e8bf878e8dc6b121fc496e5b
toktonaliev1 <toktonalievamantur110@gmail.com>
2021-11-15 23:57:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] arrays = {34, 21, 43, -14, 25, -56};
+    boolean isSorted = false;
+    while (!isSorted) {
+        isSorted = true;
+        for (int i = 1; i <arrays.length; i++) {
+            if (arrays[i] <arrays [i - 1] ) {
+                int a = arrays [i];
+                arrays [i] = arrays [i - 1];
+                arrays [i-1] = a;
+                isSorted = false;
+
+            }
+        }
+        for (int i = 0; i <arrays.length; i++) {
+            if (i > 0) {
+            }
+
+        }
+        System.out.println(Arrays.toString(arrays));
+    }
+
+
+        }
+    }
+
+
+
